9b308d38d4dd633b4d0282f23224bf570b09f510
kshetra03 <kshetra_mohan@hotmail.com>
2021-11-13 02:05:38 +0530

    add models for view, comment and link

---
+++ src/main/java/io/kprusty/reditt/model/Vote.java
@@ -0,0 +1,19 @@
+package io.kprusty.reditt.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+}
