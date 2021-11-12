672548898811c06d6cd004593558c36425cbcc84
kshetra03 <kshetra_mohan@hotmail.com>
2021-11-13 01:31:15 +0530

    initial commit

---
+++ src/main/java/io/kprusty/reditt/RedittApplication.java
@@ -0,0 +1,12 @@
+package io.kprusty.reditt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedittApplication.class, args);
+	}
+}
---
+++ src/test/java/io/kprusty/reditt/RedittApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kprusty.reditt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
